d583a0b766656eb502a3e9b2e46c1e5b9942c9f9
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-04-05 16:04:33 +0530

    update blog: ecovara-farms-review

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -575,12 +575,11 @@ export const BLOGS = [
       },
       {
         "type": "p",
-        "text": "Schedule your visit, to Ecovara Farms\n\n\n+91-8660200662\n",
+        "text": "Schedule your visit, to Ecovara Farms\n\n\n+91-866020066",
         "fontWeight": "font-bold"
       }
     ]
   },
- 
   {
     "id": 17,
     "slug": "sustainable-plantation-farmland-investment-india",
