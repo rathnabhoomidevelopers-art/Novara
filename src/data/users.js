b77c16cbe8ed58e1b3510c11a17de942ee3cf755
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 15:42:13 +0530

    remove user: sjs@gmail.com

--- src/data/users.js
+++ src/data/users.js
@@ -1,10 +1,3 @@
 export const USERS = [
-  {
-    "id": 1781852047510,
-    "email": "sjs@gmail.com",
-    "name": "srinivas",
-    "password": "sjsjasss",
-    "role": "viewer",
-    "addedOn": "19/06/2026"
-  }
+
 ];
