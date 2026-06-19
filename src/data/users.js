8f99bb2a4699bc385730f8549a744246c0682147
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 11:38:07 +0530

    remove user: gfgfgf@gmail.com

--- src/data/users.js
+++ src/data/users.js
@@ -1,10 +1,3 @@
 export const USERS = [
-  {
-    "id": 1781849261733,
-    "email": "gfgfgf@gmail.com",
-    "name": "hhfhg",
-    "password": "nkebxhyuyh",
-    "role": "viewer",
-    "addedOn": "19/06/2026"
-  }
+
 ];
