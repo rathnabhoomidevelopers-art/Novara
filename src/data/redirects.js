4db70d5328837aa35d95cdb1ed39c1bf26d41f21
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 11:38:41 +0530

    delete redirect www.redirect.com

--- src/data/redirects.js
+++ src/data/redirects.js
@@ -1,10 +1,3 @@
 export const REDIRECTS = [
-  {
-    "id": 1781849314653,
-    "from": "www.redirect.com",
-    "to": "www.manasi.com",
-    "type": "301",
-    "status": true,
-    "date": "19/06/2026"
-  }
+
 ];
