c5441d2cdf5588b0cebb66eb29c2fdf643fcbbc3
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 10:15:30 +0530

    remove user: admin@gmail.com

--- src/data/users.js
+++ src/data/users.js
@@ -1,13 +1,5 @@
 export const USERS = [
   {
-    "id": 1781844256453,
-    "email": "admin@gmail.com",
-    "name": "Admin",
-    "password": "Admin@123",
-    "role": "viewer",
-    "addedOn": "19/06/2026"
-  },
-  {
     "id": 1781844283166,
     "email": "admin2@gmail.com",
     "name": "Admin2",
