0c782858ac6b300519b04b541c24c3b946ff6d9f
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 13:00:34 +0530

    add redirect: www.hello.com -> www.hello2.com

--- src/data/redirects.js
+++ src/data/redirects.js
@@ -1,3 +1,10 @@
 export const REDIRECTS = [
-
+  {
+    "id": 1781854233757,
+    "from": "www.hello.com",
+    "to": "www.hello2.com",
+    "type": "301",
+    "status": true,
+    "date": "19/06/2026"
+  }
 ];
