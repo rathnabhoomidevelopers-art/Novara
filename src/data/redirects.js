a01da31943059173154c1c24c7c8995c2337206b
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 11:38:35 +0530

    add redirect: www.redirect.com -> www.manasi.com

--- src/data/redirects.js
+++ src/data/redirects.js
@@ -1,3 +1,10 @@
 export const REDIRECTS = [
-
+  {
+    "id": 1781849314653,
+    "from": "www.redirect.com",
+    "to": "www.manasi.com",
+    "type": "301",
+    "status": true,
+    "date": "19/06/2026"
+  }
 ];
