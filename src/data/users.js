702e77633bd1d0e2b41aafd6a2b92d184445f942
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 11:31:24 +0530

    add user: dsdssd@gmail.com (viewer)

--- src/data/users.js
+++ src/data/users.js
@@ -1,3 +1,10 @@
 export const USERS = [
-
+  {
+    "id": 1781848883518,
+    "email": "dsdssd@gmail.com",
+    "name": "srinivas",
+    "password": "srinu",
+    "role": "viewer",
+    "addedOn": "19/06/2026"
+  }
 ];
