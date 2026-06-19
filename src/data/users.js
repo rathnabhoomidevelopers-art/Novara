59acc2231cb43214e825091feb0df360249492e5
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 15:42:47 +0530

    add user: manasi.skyup@gmail.com (admin)

--- src/data/users.js
+++ src/data/users.js
@@ -1,3 +1,10 @@
 export const USERS = [
-
+  {
+    "id": 1781863966385,
+    "email": "manasi.skyup@gmail.com",
+    "name": "Manasi",
+    "password": "Manasi@0404",
+    "role": "admin",
+    "addedOn": "19/06/2026"
+  }
 ];
