ddd445946c61d3e4fd98448b22f122a467e2f12d
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 11:38:02 +0530

    update user: gfgfgf@gmail.com

--- src/data/users.js
+++ src/data/users.js
@@ -3,7 +3,7 @@ export const USERS = [
     "id": 1781849261733,
     "email": "gfgfgf@gmail.com",
     "name": "hhfhg",
-    "password": "nkebxh yuyh",
+    "password": "nkebxhyuyh",
     "role": "viewer",
     "addedOn": "19/06/2026"
   }
