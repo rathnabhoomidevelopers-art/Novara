75df7606cfb6139ade58a5ce34fd535a3e430107
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-06-19 10:14:43 +0530

    add user: admin2@gmail.com (editor)

--- src/data/users.js
+++ src/data/users.js
@@ -6,5 +6,13 @@ export const USERS = [
     "password": "Admin@123",
     "role": "viewer",
     "addedOn": "19/06/2026"
+  },
+  {
+    "id": 1781844283166,
+    "email": "admin2@gmail.com",
+    "name": "Admin2",
+    "password": "Admin@123",
+    "role": "editor",
+    "addedOn": "19/06/2026"
   }
 ];
