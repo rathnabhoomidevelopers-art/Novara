f46dff44bcb281bb62cce9fa6ac9db1a3c6c26e8
Shashikant S Bilgundi <shashibilgundi@gmail.com>
2026-06-25 15:43:16 +0530

    Update +Head.jsx

--- pages/why-novara/+Head.jsx
+++ pages/why-novara/+Head.jsx
@@ -1,25 +1,98 @@
 export default function Head() {
-  const schema = {
+  const organization = {
     "@context": "https://schema.org",
-    "@type": "AboutPage",
-    "@id": "https://novaranatureestates.com/whynovara/#aboutpage",
-    "url": "https://novaranatureestates.com/whynovara",
-    "name": "About Novara Nature Estates",
-    "description": "Learn about Novara Nature Estates, a premium managed farmland developer offering secure agricultural investment opportunities near Lepakshi, close to Bangalore.",
-    "isPartOf": {
-      "@id": "https://novaranatureestates.com/"
+    "@type": "Organization",
+    "name": "Skyup Digital Solutions LLP",
+    "url": "https://www.novaranatureestates.com/about-us",
+    "logo": "https://www.novaranatureestates.com/images/logo.svg",
+    "contactPoint": {
+      "@type": "ContactPoint",
+      "telephone": "+91-8660200662",
+      "contactType": "customer service",
+      "areaServed": "IN",
+      "availableLanguage": "en"
     },
-    "mainEntity": {
-      "@id": "https://novaranatureestates.com/#organization"
-    }
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "5",
+      "reviewCount": "120",
+      "bestRating": "5",
+      "worstRating": "1"
+    },
+    "sameAs": [
+      "https://www.facebook.com/people/Novara-Nature-Estates/61585877764871/",
+      "https://www.instagram.com/novaranatureestates/",
+      "https://www.youtube.com/@NovaraNatureEstates",
+      "https://www.linkedin.com/company/novara-nature-estates/"
+    ]
+  };
+
+  const breadcrumb = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": "1",
+        "name": "Skyup Digital Solutions",
+        "item": "https://www.novaranatureestates.com/"
+      },
+      {
+        "@type": "ListItem",
+        "position": "2",
+        "name": "Building Sustainable Farmland Investments Near Bangalore",
+        "item": "https://www.novaranatureestates.com/why-novara"
+      }
+    ]
+  };
+
+  const webpage = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "@id": "https://www.novaranatureestates.com/why-novara/#webpage",
+    "url": "https://www.novaranatureestates.com/why-novara",
+    "name": "Building Sustainable Farmland Investments Near Bangalore",
+    "description":
+      "Learn about Novara Nature Estates, a trusted developer of managed farmlands near Bangalore delivering secure, sustainable agricultural investment opportunities",
+    "isPartOf": {
+      "@type": "WebSite",
+      "@id": "https://www.novaranatureestates.com/#website",
+      "url": "https://www.novaranatureestates.com/",
+      "name": "SkyUp Digital Solutions LLP"
+    },
+    "about": {
+      "@type": "Organization",
+      "@id": "https://www.novaranatureestates.com/#organization"
+    },
+    "inLanguage": "en-IN"
   };
 
   return (
     <>
-      <link rel="canonical" href="https://www.novaranatureestates.com/whynovara" />
+      <link
+        rel="canonical"
+        href="https://www.novaranatureestates.com/why-novara"
+      />
+
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(organization),
+        }}
+      />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(breadcrumb),
+        }}
+      />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(webpage),
+        }}
       />
     </>
   );
