c7a15b996b824901cefe3baddfb6ec627bf56973
Shashikant S Bilgundi <shashibilgundi@gmail.com>
2026-06-25 15:34:23 +0530

    Update +Head.jsx

--- pages/blogs/+Head.jsx
+++ pages/blogs/+Head.jsx
@@ -1,31 +1,64 @@
 export default function Head() {
-  const blog = {
+  const webpage = {
     "@context": "https://schema.org",
-    "@type": "Blog",
-    "@id": "https://novaranatureestates.com/blogs/#blog",
-    "url": "https://novaranatureestates.com/blogs",
-    "name": "Novara Nature Estates Blog",
-    "description": "Insights and guides on managed farmland investment, farmland near Bangalore, Lepakshi farmland opportunities and agricultural land investment strategies.",
-    "publisher": {
+    "@type": "WebPage",
+    "@id": "https://www.novaranatureestates.com/blogs/#webpage",
+    "url": "https://www.novaranatureestates.com/blogs",
+    "name": "Farmland Investment Insights & Guides.",
+    "description":
+      "Explore expert insights on managed farmland investment, sustainability & agricultural land ownership near Bangalore from Novara Nature Estates.",
+    "isPartOf": {
+      "@type": "WebSite",
+      "@id": "https://www.novaranatureestates.com/#website",
+      "url": "https://www.novaranatureestates.com/",
+      "name": "Novara Nature Estates"
+    },
+    "about": {
       "@type": "Organization",
-      "@id": "https://novaranatureestates.com/#organization"
-    }
-  }
+      "@id": "https://www.novaranatureestates.com/#organization"
+    },
+    "inLanguage": "en-IN"
+  };
 
   const breadcrumb = {
     "@context": "https://schema.org",
     "@type": "BreadcrumbList",
     "itemListElement": [
-      { "@type": "ListItem", "position": 1, "name": "Home", "item": "https://novaranatureestates.com" },
-      { "@type": "ListItem", "position": 2, "name": "Blog", "item": "https://novaranatureestates.com/blogs" }
+      {
+        "@type": "ListItem",
+        "position": "1",
+        "name": "Novara Nature Estates",
+        "item": "https://www.novaranatureestates.com/"
+      },
+      {
+        "@type": "ListItem",
+        "position": "2",
+        "name": "Blogs",
+        "item": "https://www.novaranatureestates.com/blogs"
+      }
     ]
-  }
+  };
 
   return (
     <>
-      <link rel="canonical" href="https://www.novaranatureestates.com/blogs" />
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(blog) }} />
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumb) }} />
+      <link
+        rel="canonical"
+        href="https://www.novaranatureestates.com/blogs"
+      />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(webpage),
+        }}
+      />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(breadcrumb),
+        }}
+      />
     </>
-  )
+  );
 }
